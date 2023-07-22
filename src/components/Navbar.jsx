7e3cffbc95459cf60012f4cb275da5e2ca446427
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-22 18:10:43 -0400

    finally got the 3d model to appear with no console errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,9 +21,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-          <p className="text-white text-[18px] font-bold cursor-pointer md:flex ">
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Mohammad &nbsp;
-            <span className='sm:block hidden'>| Mohammad H. Rizwan</span>
+            <span className='sm:block hidden'>Rizwan</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
