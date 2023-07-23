97cb2291a0e73441812e55c8fe0f9fb6484c2c7a
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-22 20:33:16 -0400

    added loader for canvas while loading 3d model

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -11,8 +11,13 @@ const Loader = () => {
         fontSize: 14,
         color: '#f1f1f1',
         fontWeight: 800,
-        marginTop: 40
+        marginTop: 40,
+        textAlign: 'center'
       }}>{progress.toFixed(2)}%</p>
+
+      <div className="h-6 min-w-[150px] w-full bg-gray-200 rounded-full dark:bg-gray-700">
+        <div className="h-6 bg-[#20b2aa] text-md font-medium text-blue-100 text-center p-1 leading-none rounded-full" style={{ width: `${progress.toFixed(2)}%` }}>Loading... </div>
+      </div>
     </Html>
   )
 }
