81ee9f879ee6ed51c3232f667906bb501904f646
mohammad_rizwan <rizwanm76@yahoo.com>
2023-07-30 09:58:43 -0400

    added environment variables for EmailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -37,9 +38,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Mohammad Rizwan",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "mhrizwandev@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -47,7 +48,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Thank you. Your Email has been sent. I will get back to you as soon as possible.");
 
           setForm({
             name: "",
@@ -73,7 +74,7 @@ const Contact = () => {
         className='flex-[0.75] bg-tertiary p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
 
         <form
           ref={formRef}
